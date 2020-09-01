f7f5daee216ea0e9624f1189f04566c8fb1e5cf2
Jiabao-H <70572553+Jiabao-H@users.noreply.github.com>
2020-09-01 02:29:33 -0700

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+def jiabao(jax):
+    print(jax)
+
+
+# In[10]:
+
+
+jiabao('Welcome to DCSCI510!')
+
+
+# In[11]:
+
+
+print('Welcome to DCSCI510!')
+
+
+# In[ ]:
+
+
+
+
